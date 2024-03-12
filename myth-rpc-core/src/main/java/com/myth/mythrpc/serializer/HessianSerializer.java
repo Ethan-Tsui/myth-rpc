9f2a70f16bf5df6c742570fd6baa75267696d059
Ethan-Tsui <ethan_tsui@foxmail.com>
2024-03-12 18:09:03 +0800

    fix: 修正了 Hession 序列化器报错的问题

--- myth-rpc-core/src/main/java/com/myth/mythrpc/serializer/HessianSerializer.java
+++ myth-rpc-core/src/main/java/com/myth/mythrpc/serializer/HessianSerializer.java
@@ -1,5 +1,7 @@
 package com.myth.mythrpc.serializer;
 
+import com.caucho.hessian.io.Hessian2Input;
+import com.caucho.hessian.io.Hessian2Output;
 import com.caucho.hessian.io.HessianInput;
 import com.caucho.hessian.io.HessianOutput;
 
@@ -14,17 +16,35 @@ import java.io.IOException;
  */
 public class HessianSerializer implements Serializer {
     @Override
-    public <T> byte[] serialize(T object) throws IOException {
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        HessianOutput ho = new HessianOutput(bos);
-        ho.writeObject(object);
-        return bos.toByteArray();
+    public <T> byte[] serialize(T t) {
+        byte[] data = null;
+        try {
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+            Hessian2Output output = new Hessian2Output(os);
+            output.writeObject(t);
+            output.getBytesOutputStream().flush();
+            output.completeMessage();
+            output.close();
+            data = os.toByteArray();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return data;
     }
 
     @Override
-    public <T> T deserialize(byte[] bytes, Class<T> tClass) throws IOException {
-        ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
-        HessianInput hi = new HessianInput(bis);
-        return (T) hi.readObject(tClass);
+    public <T> T deserialize(byte[] data, Class<T> clazz) {
+        if (data == null) {
+            return null;
+        }
+        Object result = null;
+        try {
+            ByteArrayInputStream is = new ByteArrayInputStream(data);
+            Hessian2Input input = new Hessian2Input(is);
+            result = input.readObject();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return (T) result;
     }
 }
